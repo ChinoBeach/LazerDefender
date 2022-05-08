8da92d51af8afa54f0198093bb4a2e34c26035d0
ChinoBeach <beach988@morrisville.edu>
2022-05-07 22:28:58 -0400

    Pathfinding

---
+++ Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+    [SerializeField] WaveConfigSO waveConfig;
+    List<Transform> waypoints;
+    int intWaypointIndex= 0 ;
+    void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[intWaypointIndex].position;
+    }
+
+  
+    void Update()
+    {
+        if(intWaypointIndex<waypoints.Count)
+        {
+            Vector3 targetPosition = waypoints[intWaypointIndex].position;
+            float fltDelta = waveConfig.GetMoveSpeed() * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, fltDelta);
+            if(transform.position == targetPosition)
+            {
+                intWaypointIndex++;
+            }
+
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
